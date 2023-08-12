3589f3f40d53dde444be2886137f16b07d1ee8f1
samani siewe darix <samanidarix@gmail.com>
2023-08-12 10:24:29 +0100

    add parameter

--- src/CollectMiddleware/middleware.py
+++ src/CollectMiddleware/middleware.py
@@ -1,6 +1,7 @@
 from starlette import Request
 from starlette.middleware.base import BaseHTTPMiddleware
 from redis import Redis
+from datetime import datetime
 
 class CollectMiddleware(BaseHTTPMiddleware):
     def __init__(self,app,redis_host: str, redis_port: str):
@@ -13,10 +14,20 @@ class CollectMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next):
         # do something with the request object, for example
         # i want to collect data from Request object to redisTimeseris iwth this midddleware
-
-        content_type = request.headers.get('Content-Type')
-        print(f"content_type: {content_type}")
         
+        client_host = request.client.host
+        rewuest_path = request.url.path
+        request_methode = request.method
+        schema_request = request.url.scheme
+        content_type = request.headers.get('Content-Type')
+        header_request = request.headers
+        request_port = request.url.port
+        query_parametre_request = request.query_params
+        path_parametre_request = request.path_params
+        cokies_request = request.cookies
+        boby_request = await request.json()
+
+        time = datetime.now()
         # process the request and get the response    
         response = await call_next(request)
         
