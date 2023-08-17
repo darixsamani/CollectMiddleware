525ccbfdbdc4f32c0780ee7198851a5b4329f5db
samani siewe darix <samanidarix@gmail.com>
2023-08-17 06:21:43 +0100

    add more parameter

--- src/CollectMiddleware/middleware.py
+++ src/CollectMiddleware/middleware.py
@@ -16,17 +16,22 @@ class CollectMiddleware(BaseHTTPMiddleware):
         # i want to collect data from Request object to redisTimeseris iwth this midddleware
         
         client_host = request.client.host
-        rewuest_path = request.url.path
+        request_path = request.url.path
         request_methode = request.method
         schema_request = request.url.scheme
         content_type = request.headers.get('Content-Type')
+        user_agent = request.headers.get("user-agent")
+        host = request.headers.get("host")
+        connection = request.headers.get("connection")
+        content_length = request.headers.get("content-length")
+        referer = request.headers.get("referer")
+        accept_encoding = request.headers.get("accept-encoding")
+        origin = request.headers.ge("origin")
         header_request = request.headers
         request_port = request.url.port
         query_parametre_request = request.query_params
         path_parametre_request = request.path_params
         cokies_request = request.cookies
-        boby_request = await request.json()
-
         time = datetime.now()
         # process the request and get the response    
         response = await call_next(request)
