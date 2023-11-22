919f57ee55dc6d6578b5151bd0d8b004bc0294b9
samani siewe darix <samanidarix@gmail.com>
2023-11-22 19:29:13 +0100

    working version

--- src/CollectMiddleware/middleware.py
+++ src/CollectMiddleware/middleware.py
@@ -1,15 +1,15 @@
 from starlette import Request
 from starlette.middleware.base import BaseHTTPMiddleware
-from redis import Redis
+from pymongo import MongoClient
 from datetime import datetime
 
 class CollectMiddleware(BaseHTTPMiddleware):
-    def __init__(self,app,redis_host: str, redis_port: str):
+    def __init__(self,app,mongodb_uri: str,):
         
         super().__init__(app)
-        self.redis_host = redis_host
-        self.redis_port = redis_port
-        self.redis = Redis(self.redis_port, redis_host)
+        self.client = MongoClient(self.mongodb_uri)
+        self.db = self.client["CollectMiddleware"]
+        self.collection = self.db.CollectMiddleware
 
     async def dispatch(self, request: Request, call_next):
         # do something with the request object, for example
@@ -19,12 +19,12 @@ class CollectMiddleware(BaseHTTPMiddleware):
         request_path = request.url.path
         request_methode = request.method
         schema_request = request.url.scheme
+        port = request.url.port
         content_type = request.headers.get('Content-Type')
         user_agent = request.headers.get("user-agent")
         host = request.headers.get("host")
         connection = request.headers.get("connection")
-        content_length = request.headers.get("content-length")
-        referer = request.headers.get("referer")
+        referer = reqort = request.headers.get("referer")
         accept_encoding = request.headers.get("accept-encoding")
         origin = request.headers.get("origin")
         request_port = request.url.port
@@ -36,7 +36,38 @@ class CollectMiddleware(BaseHTTPMiddleware):
         authentification = request.headers.get("authorization")
         content_length = request.headers.get("content-length")
         accept_language = request.headers.get("accept-language")
+        boby = await request.body()
         time = datetime.now()
+
+        data = {
+            "time": time,
+            "client_host": client_host,
+            "request_methode": request_methode,
+            "schema_request": schema_request,
+            "port": port,
+            "content_type": content_type,
+            "user_agent": user_agent,
+            "host": host,
+            "connection": connection,
+            "content_length": content_length,
+            "referer": referer,
+            "accept_encoding": accept_encoding,
+            "origin": origin,
+            "request_port": request_port,
+            "query_parametre_request": query_parametre_request,
+            "path_parametre_request": path_parametre_request,
+            "cokies_request": cokies_request,
+            "accept": accept,
+            "content_type": content_type,
+            "authentification": authentification,
+            "accept_language": accept_language,
+            "boby": boby
+        }
+
+        self.collection.insert_one(data)
+
+
+        response = await call_next(request)
 
         
         return response
